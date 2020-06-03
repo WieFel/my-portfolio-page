a926815b1773461321960e3eea18c0144c9690ae
Felix Wielander <felix.wielander@gmail.com>
2020-06-03 16:21:15 +0200

    Adapted helmet file images

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -41,57 +41,39 @@ const Helmet = ({ theme = {} }) => (
           <meta charSet="utf-8" />
           <title>{title}</title>
           <meta name="description" content={description} />
-          <link
-            rel="shortcut icon"
-            href={`https:${childImageSharp.favicon32.src}`}
-          />
+          <link rel="shortcut icon" href={childImageSharp.favicon32.src} />
           <meta name="theme-color" content={theme.background} />
-          <meta
-            name="image"
-            content={`https:${childImageSharp.favicon32.src}`}
-          />
+          <meta name="image" content={childImageSharp.favicon32.src} />
           <meta itemProp="name" content={title} />
           <meta itemProp="description" content={description} />
-          <meta
-            itemProp="image"
-            content={`https:${childImageSharp.favicon32.src}`}
-          />
+          <meta itemProp="image" content={childImageSharp.favicon32.src} />
           <meta name="og:title" content={title} />
           <meta name="og:description" content={description} />
-          <meta
-            name="og:image"
-            content={`https:${childImageSharp.bigIcon.src}`}
-          />
+          <meta name="og:image" content={childImageSharp.bigIcon.src} />
           <meta name="og:site_name" content={title} />
           <meta name="og:locale" content="en_US" />
           <meta name="og:type" content="website" />
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:title" content={title} />
-          <meta name="twitter:description" content={description} />
-          <meta
-            name="twitter:image"
-            content={`https:${childImageSharp.bigIcon.src}`}
-          />
+          <meta name="twitter:image" content={childImageSharp.bigIcon.src} />
           <meta
             name="twitter:image:src"
-            content={`https:${childImageSharp.bigIcon.src}`}
+            content={childImageSharp.bigIcon.src}
           />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href={`https:${childImageSharp.appleIcon.src}`}
+            href={childImageSharp.appleIcon.src}
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href={`https:${childImageSharp.favicon32.src}`}
+            href={childImageSharp.favicon32.src}
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href={`https:${childImageSharp.favicon16.src}`}
+            href={childImageSharp.favicon16.src}
           />
         </ReactHelmet>
       );
